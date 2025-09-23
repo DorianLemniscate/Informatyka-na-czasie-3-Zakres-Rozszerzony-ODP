f45d40bab9115e5ef83a86532baaff4384faf317
DorianLemniscate <dorianfamitsu@gmail.com>
2025-09-23 10:05:33 +0200

    Add files via upload

---
+++ Odwrotna Notacja Polska (ONP)/Zad3Str26.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+int W(string onp)
+{
+    stack<int>s;
+    for(int i=0; i<onp.size(); i++)
+    if (onp[i]>='0' && onp[i]<='9')
+    s.push(onp[i]-'0');
+    else
+    {
+        int arg2=s.top(); s.pop();
+        int arg1=s.top(); s.pop();
+        switch (onp[i])
+        {
+            case '+':
+            s.push(arg1+arg2);
+            break;
+            
+            case '-':
+            s.push(arg1-arg2);
+            break;
+
+            case '*':
+            s.push(arg1*arg2);
+            break;
+
+            case '/':
+            s.push(arg1/arg2);
+            break;
+
+            case '%':
+            s.push(arg1%arg2);
+
+        }
+    }
+
+    int ONP=s.top();
+    s.pop();
+    return ONP;
+
+}
+
+int main()
+{
+    string onp;
+    cout<<"Podaj wyrazenie: "; cin>>onp;
+
+    cout<<"Wynik: " << W(onp);
+    return 0;
+
+}
+
+//Zadanie 3 Strona 26
